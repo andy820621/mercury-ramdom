c4b210ff5eec0d24014a0c5288e83c16b3a75058
BarZ <andy820621@gmail.com>
2023-09-20 09:32:26 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,249 @@
-<script setup lang="ts">
-import { ref } from "vue";
-const startNumber = ref(75);
-const endNumber = ref(100);
-const count = ref(1);
-const randomNumbers = ref<number[]>([]);
-
-const generateRandomNumbers = () => {
-	randomNumbers.value = [];
-
-	for (let i = 0; i < count.value; i++) {
-		const randomNumber = Math.floor(
-			Math.random() * (endNumber.value - startNumber.value + 1) +
-				startNumber.value
-		);
-		randomNumbers.value.push(randomNumber);
-	}
-};
-</script>
-
 <template>
-	<header>
-		<img alt="Vue logo" class="logo" src="./assets/icon.png" />
-		<h1>水星防火工程</h1>
-	</header>
-
-	<main class="main">
-		<div class="random-box">
-			<h2>亂數生成器</h2>
-			<div class="startToEnd">
-				<div class="inputBox">
-					<label for="start">起始數字: </label>
-					<input id="start" type="number" v-model="startNumber" />
-				</div>
-				<div class="inputBox">
-					<label for="end">結束數字: </label>
-					<input id="end" type="number" v-model="endNumber" />
-				</div>
+	<div class="main-content">
+		<div class="img-box">
+			<div class="circle">
+				<ul class="selected-number-array">
+					<li
+						v-for="(selectedNumbers, index) in selectedNumbersArray"
+						:key="index"
+					>
+						第{{ index + 1 }}組
+						<ul class="numberArray">
+							<li v-for="(number, chIndex) in selectedNumbers" :key="chIndex">
+								{{ number }}
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<img class="main-img" src="./assets/0929中秋節 賓果抽獎.png" alt="" />
+			<div class="current-number">
+				<p v-if="alertMessage">{{ alertMessage }}</p>
+				<ul v-else class="numberArray">
+					<li v-for="(number, index) in randomNumbers" :key="index">
+						{{ number }}
+					</li>
+				</ul>
+			</div>
+			<div class="btn" @click.prevent="generateRandomNumbers">
+				<img src="./assets/logo.png" alt="" />
 			</div>
 			<div class="inputBox">
 				<label for="count">需要產出的數字個數: </label>
-				<input id="count" type="number" v-model="count" />
+				<input id="count" type="number" v-model="count" max="5" />
 			</div>
-			<button class="btn" @click.prevent="generateRandomNumbers">
-				生成亂數
-			</button>
-		</div>
 
-		<div class="content" v-if="randomNumbers.length > 0">
-			<h2>隨機數字:</h2>
-			<ul>
-				<li v-for="number in randomNumbers" :key="number">{{ number }}</li>
-			</ul>
+			<button class="restart" @click="confirmRestart">重新開始</button>
 		</div>
-	</main>
+	</div>
 </template>
 
+<script setup lang="ts">
+import { ref, watch, onMounted, computed } from "vue";
+const startNumber = ref(1);
+const endNumber = ref(75);
+const numberArray = ref<number[]>([]);
+for (let i = startNumber.value; i <= endNumber.value; i++) {
+	numberArray.value.push(i);
+}
+const count = ref(3);
+const randomNumbers = ref<number[]>([]);
+const selectedNumbersArray = ref<number[][]>([]);
+const alertMessage = computed(() =>
+	numberArray.value.length ? "" : "已經沒有數字可以產生了!!!"
+);
+
+watch(count, (val) => {
+	if (val) {
+		if (val > 5) {
+			count.value = 5;
+		} else if (val < 1) {
+			count.value = 1;
+		}
+	}
+});
+
+onMounted(() => {
+	const numsArray = localStorage.getItem("selectedNumbersArray");
+	if (numsArray) {
+		let storageCount = 0;
+		JSON.parse(numsArray).forEach((numsArr: number[]) => {
+			numsArr.forEach((num: number) => {
+				storageCount++;
+			});
+		});
+		if (storageCount === endNumber.value - startNumber.value + 1) {
+			numberArray.value.length = 0;
+		}
+		selectedNumbersArray.value = JSON.parse(numsArray);
+	}
+});
+
+const generateRandomNumbers = () => {
+	if (alertMessage.value.length) return;
+
+	randomNumbers.value = [];
+
+	while (
+		randomNumbers.value.length < count.value &&
+		numberArray.value.length > 0
+	) {
+		const randomIndex = Math.floor(Math.random() * numberArray.value.length);
+
+		const selectedNumber = numberArray.value.splice(randomIndex, 1)[0];
+
+		randomNumbers.value.push(selectedNumber);
+	}
+	// 添加到 selectedNumbersArray 中
+	selectedNumbersArray.value.push(randomNumbers.value);
+
+	// 更新本地存储
+	localStorage.setItem(
+		"selectedNumbersArray",
+		JSON.stringify(selectedNumbersArray.value)
+	);
+};
+
+function confirmRestart() {
+	const shouldRestart = window.confirm("是否要重新開始？");
+	if (shouldRestart) {
+		localStorage.removeItem("selectedNumbersArray");
+		window.location.reload();
+	}
+}
+</script>
+
 <style scoped lang="scss">
-header {
+.main-content {
+	width: 100%;
+	height: 100vh;
 	display: flex;
-}
+	justify-content: center;
+	.img-box {
+		position: relative;
+		.current-number,
+		.circle,
+		.btn,
+		.inputBox,
+		.restart {
+			position: absolute;
+		}
+		.restart {
+			bottom: 1rem;
+			left: 50%;
+			transform: translateX(-50%);
+			z-index: 3;
+			// 按鈕樣式
+			background-color: #ff6600;
+			color: #fff;
+			padding: 10px 20px;
+			border: none;
+			border-radius: 5px;
+			cursor: pointer;
+			transition: background-color 0.3s ease;
 
-.logo {
-	display: block;
-	margin-left: 1rem;
-	margin-right: 1rem;
-	width: 2.4rem;
-	height: 2.4rem;
-}
+			&:hover {
+				background-color: #ff8800;
+			}
 
-h1 {
-	font-size: 1.5rem;
-	margin: 0;
-}
-.content,
-.random-box {
-	margin: 0 auto;
-	display: flex;
-	flex-direction: column;
-	width: 800px;
-	align-items: center;
-	gap: 1rem;
-	font-size: 1.2rem;
-}
+			&:active {
+				background-color: #ff4400;
+				transform: translateX(-50%) scale(0.95);
+			}
+		}
+		.inputBox {
+			width: 10%;
+			height: 100px;
+			left: 5%;
+			bottom: 8%;
+			z-index: 3;
+			display: grid;
+			label {
+				display: none;
+			}
+			input {
+				text-align: center;
+				width: 100%;
+				border: none;
+				outline: none;
+				padding: 0;
+				margin: 0;
+				background: transparent;
+				font-size: 2.8rem;
+				color: #000;
+			}
+			input[type="number"]::-webkit-inner-spin-button,
+			input[type="number"]::-webkit-outer-spin-button {
+				-webkit-appearance: none;
+				appearance: none;
+				display: none;
+			}
+		}
+		.btn {
+			width: 10%;
+			aspect-ratio: 10 / 14;
+			bottom: 2.4%;
+			right: 2.4%;
+			z-index: 3;
+			cursor: pointer;
+			&:active {
+				transform: scale(0.95);
+			}
+			img {
+				width: 100%;
+				object-fit: cover;
+				object-position: center;
+			}
+		}
+		.current-number {
+			width: 20.2%;
+			height: 25.6%;
+			top: 9.88%;
+			right: 1.9%;
+			z-index: 3;
+			display: grid;
+			place-items: center;
+		}
+		.circle {
+			position: absolute;
+			width: 49.8%;
+			aspect-ratio: 1;
+			top: 3.1%;
+			left: 3.3%;
+			z-index: 1;
+			background-image: linear-gradient(
+				90deg,
+				#fffbc6 0%,
+				#fff22c 50%,
+				#fff100 100%
+			);
+			border-radius: 50%;
+			overflow: hidden;
 
-.random-box {
-	margin-top: 1rem;
-	label {
-		display: inline-block;
-		width: 190px;
-		text-align: left;
-		margin-right: 10px;
-	}
-
-	input {
-		padding: 5px;
-		font-size: 16px;
-	}
-	.startToEnd {
-		display: grid;
-		gap: 1rem;
-	}
-	.btn {
-		padding: 10px 20px;
-		font-size: 18px;
-		background-color: #007bff;
-		color: #fff;
-		border: none;
-		cursor: pointer;
-		margin-top: 1rem;
-		border-radius: 5px;
-		&:hover {
-			background-color: #0056b3;
+			.selected-number-array {
+				width: 50%;
+				min-width: 410px;
+				margin: 5rem auto;
+				height: calc(100% - 10rem);
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-start;
+				align-items: center;
+				overflow: auto;
+				& > li {
+					display: flex;
+					align-items: center;
+					gap: 1rem;
+				}
+			}
+		}
+		.main-img {
+			position: relative;
+			height: 100%;
+			object-fit: cover;
+			object-position: center;
+			z-index: 2;
+			pointer-events: none;
 		}
 	}
 }
@@ -125,6 +258,18 @@ li {
 	font-size: 1.5rem;
 }
 
-/* @media (min-width: 1024px) {
-} */
+.numberArray {
+	display: flex;
+	gap: 0.8rem;
+	li {
+		width: 50px;
+		height: 50px;
+		font-size: 1.5rem;
+		font-weight: 800;
+		border: 1px solid #000;
+		border-radius: 50%;
+		display: grid;
+		place-items: center;
+	}
+}
 </style>
